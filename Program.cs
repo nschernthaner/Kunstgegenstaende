a20d22ffc6be07d8cdac5c1b04556443565b9e4a
Nico Schernthaner <nschernthaner@schueler.hakzell.at>
2024-02-20 11:02:00 +0100

    kunstgegenstände hinzugefügt

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Kunstgegenstände;
+
+Kunstgegenstand mensch = new("Mensch");
+Kunstgegenstand tisch = new("Tisch", true);
+Kunstgegenstand ball = new("Ball");
+Kunstgegenstand tischGroß = new("Tisch groß", true);
