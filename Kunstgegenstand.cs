e48a89026d30c999ba9423514c0022a1c1fc306c
Nico Schernthaner <nschernthaner@schueler.hakzell.at>
2024-02-20 10:51:47 +0100

    Preismethoden hinzugefügt

--- Kunstgegenstand.cs
+++ Kunstgegenstand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Kunstgegenstände;
 /// <summary>
@@ -27,11 +28,32 @@ public class Kunstgegenstand
     /// Erstellt einen Kunstgegenstand mit Namen und festlegung ob er verkaufbar ist.
     /// </summary>
     /// <param name="titel">Der Name des Kunstgegenstandes
-    /// <param name="verkaufbar">Festlegung
+    /// <param name="verkaufbar">Festlegung, ob er verkaufbar ist.
     public Kunstgegenstand(string titel, bool verkaufbar) 
     { 
         _titel = titel;
         _verkaufbar = verkaufbar;
+    }
+
+    public string GetTitel() 
+    {
+        return _titel;
+    }
+    /// <summary>
+    /// Liest den Preis aus
+    /// </summary>
+    /// <returns>Der Wert des Kunstgegenstand wird zurückgegeben</returns>
+    public double GetPreis() 
+    { 
+       return _preis;
+    }
+    /// <summary>
+    /// Setzt den Preis
+    /// </summary>
+    /// <param name="preis">Der Wert des Kunstgegenstandes</param>
+    public void SetPreis(double preis) 
+    {
+        _preis = preis;
     }
 
 }
