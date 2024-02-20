a2ab26c87fdafa13339c94ea5a280e7ef1ab64ca
Nico Schernthaner <nschernthaner@schueler.hakzell.at>
2024-02-20 10:45:15 +0100

    Konstruktor hinzugefügt

--- Kunstgegenstand.cs
+++ Kunstgegenstand.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Kunstgegenstände;
-
+/// <summary>
+/// Erstellt einen Kunstgegenstand
+/// </summary>
 public class Kunstgegenstand
 {
     string _titel;
@@ -13,4 +15,23 @@ public class Kunstgegenstand
     bool _verkaufbar = false;
 
     double _preis = 0;
+    /// <summary>
+    /// Erstellt einen Kunstgegenstand mit Namen
+    /// </summary>
+    /// <param name="titel">Der Name des Kunstgegenstandes
+    public Kunstgegenstand(string titel) 
+    {
+        _titel = titel;
+    }
+    /// <summary>
+    /// Erstellt einen Kunstgegenstand mit Namen und festlegung ob er verkaufbar ist.
+    /// </summary>
+    /// <param name="titel">Der Name des Kunstgegenstandes
+    /// <param name="verkaufbar">Festlegung
+    public Kunstgegenstand(string titel, bool verkaufbar) 
+    { 
+        _titel = titel;
+        _verkaufbar = verkaufbar;
+    }
+
 }
